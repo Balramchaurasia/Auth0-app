213a6369d9b3bf6089fff17f8721eaf350a8d93f
Balram Chaurasia <120183066+Balramchaurasia@users.noreply.github.com>
2025-03-25 22:59:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ import authRouter from "./src/routes/auth/route.js";
 const app=express();
 app.use(express.json());
 app.use(cors({
-    origin: ["https://auth0-app-three.vercel.app"]
+    origin:"*"
   }));
 app.use("/auth",authRouter);
 
